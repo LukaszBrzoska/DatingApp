05081832ddc93abcc3edcad7ab4a788630bbb864
Lukasz <s16399@pjwstk.edu.pl>
2020-01-18 21:17:28 +0100

    Paging , Sorting , Filtering in the API/SPA

--- DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
+++ DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
@@ -8,11 +8,14 @@ import { User } from '../_models/user';
 
 @Injectable()
 export class MemberListResolver implements Resolve<User[]> {
+    pageNumber = 1;
+    pageSize = 5;
+
     constructor(private userService: UserService,
                 private router: Router, private alerrify: AlertifyService) {}
 
     resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
-        return this.userService.getUsers().pipe(
+        return this.userService.getUsers(this.pageNumber, this.pageSize).pipe(
             catchError(error => {
             this.alerrify.error('Problem retrieving data');
             this.router.navigate(['/home']);
