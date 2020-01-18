9fd5a1b65538b684d1f848984a74ca688a2677ca
Lukasz <s16399@pjwstk.edu.pl>
2020-01-18 15:09:31 +0100

    registration validation

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -36,14 +36,13 @@ namespace DatingApp.API.Controllers
             if (await _repo.UserExists(userForRegisterDto.Username))
                 return BadRequest("Username already exists");
 
-            var userToCreate = new User
-            {
-                Username = userForRegisterDto.Username
-            };
+            var userToCreate = _mapper.Map<User>(userForRegisterDto);
 
-            var createUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
-            return StatusCode(201);
+            var userToReturn = _mapper.Map<UserForDetailedDto>(createdUser);
+
+            return CreatedAtRoute("GetUser", new {controller = "Users", id = createdUser.Id}, userToReturn);
         }
 
         [HttpPost("login")]
@@ -81,7 +80,7 @@ namespace DatingApp.API.Controllers
 
             return Ok(new
             {
-                token = tokenHandler.WriteToken(token), 
+                token = tokenHandler.WriteToken(token),
                 user
             });
 
